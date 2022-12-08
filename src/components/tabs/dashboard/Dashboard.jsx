b5ee0f98670c939d3c1172a9b6639287e91d19aa
unknown <souissi13mouldi@gmail.com>
2022-12-08 23:18:28 +0100

    done

--- src/components/tabs/dashboard/Dashboard.jsx
+++ src/components/tabs/dashboard/Dashboard.jsx
@@ -1,17 +1,21 @@
+import "./dashboard_styles.css";
 import React from "react";
 
 const Dashboard = () => {
   const general = [
-    { title: "Fond de caisse", value: 1500, icon: "fa-user" },
-    { title: "Fond Bet 24", value: 1500, icon: "fa-user" },
-    { title: "Fond Max bet", value: 1500, icon: "fa-user" },
+    { title: "Fond de caisse", value: 1500, icon: "fa-solid fa-landmark" },
+    { title: "Fond Bet 24", value: 900, icon: "fa-solid fa-landmark" },
+    { title: "Fond Max bet", value: 750, icon: "fa-solid fa-landmark" },
   ];
   return (
-    <div className="row justify-content-center">
+    <div className="dashboard_cards">
       {general.map((doc, i) => (
-        <div className="card shadow p-4 col-lg-3 dashboard_card" key={i}>
+        <div className="dashboard_card" key={i}>
           <div className="card_title">{doc.title}</div>
-          <div className="card_value">{doc.value}</div>
+          <div className="d-flex justify-content-between w-100 align-items-center">
+            <i className={doc.icon}></i>
+            <div className="card_value">{doc.value}</div>
+          </div>
         </div>
       ))}
     </div>
